ae2ff26539f8a56c12a1ec3dc9058d65bfa68048
Carlos Urquiza <webquiza@gmail.com>
2021-09-05 20:22:20 -0700

    print Buzz if number is divisible by 5

--- scripts.js
+++ scripts.js
@@ -4,7 +4,9 @@ let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz
 
 for (let i = 1; i <= answer; i++) {
   if (i % 3 === 0) {
-    console.log("Fizz")
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
   } else {
     console.log(i);
   }
