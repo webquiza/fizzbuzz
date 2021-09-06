6252943f5bb4f3f393794deb51311147369df634
Carlos Urquiza <webquiza@gmail.com>
2021-09-05 20:19:23 -0700

    print Fizz if number is divisible by 3

--- scripts.js
+++ scripts.js
@@ -3,5 +3,9 @@
 let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
 
 for (let i = 1; i <= answer; i++) {
-  console.log(i);
+  if (i % 3 === 0) {
+    console.log("Fizz")
+  } else {
+    console.log(i);
+  }
 }
