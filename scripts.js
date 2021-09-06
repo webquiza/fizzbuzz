841788fa59cfc91b1e338b4aa185e8960cefdac8
Carlos Urquiza <webquiza@gmail.com>
2021-09-05 20:15:20 -0700

    add for loop to loop from 1 to the entered #

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
 // Get input from user and wrap prompt call in a parseInt function so number is returned.
 
 let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+for (let i = 1; i <= answer; i++) {
+  console.log(i);
+}
